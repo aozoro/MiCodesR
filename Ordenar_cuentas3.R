df049913fe3ce59aa8592ef78d1526acacf3a769
OMAR DE LA SOTA <aozoro@gmail.com>
2018-10-22 14:18:02 -0500

    Actualización 'BuscarCuenta.xlsx'
    mejora en el tiempo de ejecución

--- Ordenar_cuentas3.R
+++ Ordenar_cuentas3.R
@@ -16,11 +16,14 @@ cartera <- read_delim(myfile,"|", escape_double = FALSE,
                                        APELLIDO_PATERNO = col_character(), FECHA_FACTURACION=col_integer()), 
                       trim_ws = TRUE, locale=locale(encoding = "latin1"))
 
-cartera <- data.frame(cartera[,"CUENTA_SAT"],cartera[,"NUM_DOCUMENTO"], cartera[,"NOMBRES"],
-                      cartera[,"APELLIDO_PATERNO"],cartera[,"APELLIDO_MATERNO"], 
-                      cartera[,"FECHA_FACTURACION"])
-
+cartera <- select(cartera,CUENTA_SAT,NUM_DOCUMENTO,NOMBRES,APELLIDO_PATERNO,APELLIDO_MATERNO,FECHA_FACTURACION)
 colnames(cartera)[1:6] <- c("CUENTA","DNI","NOMBRES", "APELLIDO_P","APELLIDO_M","DIAFACTURACION")
+
+Nombre_Completo <- data.frame(ifelse(cartera$APELLIDO_M=="X",paste(cartera$NOMBRES,cartera$APELLIDO_P),paste(cartera$NOMBRES,cartera$APELLIDO_P,cartera$APELLIDO_M)))
+colnames(Nombre_Completo) <- "NOMBRE_COMPLETO"
+
+cartera<- select(cartera, -NOMBRES, -APELLIDO_P, -APELLIDO_M)
+cartera<- data.frame(cartera[,1:2],Nombre_Completo, cartera[,3:ncol(cartera)])
 cartera$CUENTA <- ifelse(cartera$CUENTA > 10^11, trunc(cartera$CUENTA/100) , cartera$CUENTA )
 cartera <-  arrange(cartera , CUENTA)
 
@@ -31,7 +34,10 @@ t=trunc(N/stp)
 nh=ceiling(N/stp)
 Lim <- c()
 
-for (i in 1:t){
+i<-0
+
+if (t>=1){
+  for (i in 1:t){
     first=1+stp*(i-1)
     last = stp*i
     hoja<-data.frame(cartera[first:last,])
@@ -39,17 +45,18 @@ for (i in 1:t){
     wsName <-paste0("Hoja",i)
     addWorksheet(wb,wsName)
     writeData(wb,wsName,hoja)   
-} 
+  } 
+}
 
 if(t!=nh){
-    i=i+1
-    first=1+stp*(i-1)
-    last=N
-    hoja<-data.frame(cartera[first:last,])
-    Lim[i]<-hoja[1,1]
-    wsName <-paste0("Hoja",i)
-    addWorksheet(wb,wsName)
-    writeData(wb,wsName,hoja)
+  i=i+1
+  first=1+stp*(i-1)
+  last=N
+  hoja<-data.frame(cartera[first:last,])
+  Lim[i]<-hoja[1,1]
+  wsName <-paste0("Hoja",i)
+  addWorksheet(wb,wsName)
+  writeData(wb,wsName,hoja)
 }
 
 i=i+1
