367a9ea8672911fce834a4a5800597e2988b3bd6
OMAR DE LA SOTA <aozoro@gmail.com>
2018-10-26 10:18:30 -0500

    Cuentas de 10 digitos

--- CuentasCerradas.R
+++ CuentasCerradas.R
@@ -30,6 +30,7 @@ cartera<- data.frame(cartera[,1:2],Nombre_Completo, cartera[,3:ncol(cartera)])
 
 rm("Nombre_Completo")
 
+cartera$CUENTA <- ifelse(cartera$CUENTA > 10^11, trunc(cartera$CUENTA/100) , cartera$CUENTA )
 cartera<-arrange(cartera,desc(FECHACIERRE))
 
 wb <- createWorkbook()
