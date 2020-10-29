515ffe558ed49058a84aa5af7b54d0dfb87c7041
Omar de la Sota <aozoro@gmail.com>
2020-10-29 10:55:25 -0500

    install_packages
    Install.packages + library funcion

---
+++ miR.R
@@ -0,0 +1,15 @@
+install_packages<-function(list.of.packages){
+    if (!is.vector(list.of.packages)){
+        list.of.packages<-c(list.of.packages)
+    }
+    
+    new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+    
+    if(length(new.packages)) {
+        install.packages(new.packages)
+    }
+
+    for(package in list.of.packages){
+        library(package,character.only = TRUE)
+    }
+}
